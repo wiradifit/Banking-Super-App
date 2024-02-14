f54adbecb4be8e87f6c29edba11c4551ada3be37
Prawira Hadi Fitrajaya <fitrajayaprawira@gmail.com>
2024-02-14 20:56:07 +0700

    Initial Commit

---
+++ BankingSuperApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankingSuperApp
+//
+//  Created by Prawira Hadi Fitrajaya on 14/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
